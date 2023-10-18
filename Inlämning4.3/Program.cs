77258c867bb6d471ba1ac8865ca28f813dc58885
isel06002 <isel06002@E-PW0234HX>
2023-10-18 10:37:24 +0200

    Add project files.

---
+++ Inlämning4.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Inlämning_4.__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<101;i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i%5==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
